dd063e745fe5e596b530a4e1256c71ae066f00b3
DESKTOP-I6DDE09\1121s <ji>
2022-10-12 20:38:02 +0900

    결재 아웃라인 생성

---
+++ workspace/checkminePrj/src/main/java/com/kh/checkmine/approval/controller/ApprovalController.java
@@ -0,0 +1,17 @@
+package com.kh.checkmine.approval.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("approval")
+public class ApprovalController {
+	
+	@GetMapping("draft")
+	public String draft() {
+		
+		return "approval/draft";
+	}
+
+}
